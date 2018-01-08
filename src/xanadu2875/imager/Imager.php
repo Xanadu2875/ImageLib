c88be6876ff4297de81c0e1166e7f42a8981902d
UramnOIL <uramnoil.outlook.com>
2018-01-08 11:12:13 +0900

    いろいろ

---
+++ src/xanadu2875/imager/Imager.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace xanadu2875\imager;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\{Config, TextFormat as TF, Color};
+use pocketmine\event;
+use pocketmine\item\Item;
+use pocketmine\network\mcpe\protocol\ClientboundMapItemDataPacket;
+use pocketmine\nbt\tag\StringTag;
+define("RAND_MAX", 1000000);
+define("WIDTH", 127);
+define("HEIGHT", 127);
+
+class Imager extends PluginBase implements event\Listener
+{
+  public $images = [];
+
+  public function onLoad()
+  {
+    @mkdir($this->getDataFolder(), 777);
+    @mkdir($this->getDataFolder() . "image/", 777);
+
+    $config = ($source = new Config($this->getDataFolder() . "images.yml", Config::YAML))->getAll();
+    $images = glob($this->getDataFolder() . "image/{*.png,*.jpg,*.jpeg}", GLOB_BRACE);
+
+    foreach($images as $key => $filename)
+    {
+      if(!isset($config[$filename]))
+      {
+        $config[$filename] = mt_rand(0, RAND_MAX);
+      }
+    }
+
+    foreach($config as $key0 => $value)
+    {
+      $result = false;
+
+      foreach($images as $key1 => $filename)
+      {
+        if($filename === $key0)
+        {
+          $result = true;
+          unset($images[$key1]);
+          break;
+        }
+      }
+
+      if(!$result)
+      {
+        unset($config[$key0]);
+      }
+    }
+
+    $source->setAll($config);
+    $source->save();
+
+    foreach($config as $image => $id)
+    {
+      $colors = [];
+
+      switch(pathinfo($image)["extension"])
+      {
+        case "png":
+          if(!$image = @imagecreatefrompng($image)) { continue; }
+          break;
+        case "jpg":
+        case "jpeg":
+          if(!$image = @imagecreatefromjpeg($image)) { continue; }
+          break;
+        default:
+          continue;
+      }
+
+      var_dump($image);
+
+      $image = imagescale($image, WIDTH, HEIGHT, IMG_NEAREST_NEIGHBOUR);
+
+      for($y = 0;$y < WIDTH; ++$y)
+      {
+        for($x = 0;$x < HEIGHT; ++$x)
+        {
+          $rgb = imagecolorsforindex($image, imagecolorat($image, $x, $y));
+          $colors[$y][$x] = new Color($rgb["red"], $rgb["green"], $rgb["blue"], 0xff);
+        }
+      }
+
+      $pk = new ClientboundMapItemDataPacket();
+      $pk->mapId = $id;
+      $pk->type = ClientboundMapItemDataPacket::BITFLAG_TEXTURE_UPDATE;
+      $pk->height = HEIGHT;
+      $pk->width = WIDTH;
+      $pk->scale = 1;
+      $pk->colors = $colors;
+
+      $this->images[(int)$id] = $pk;
+    }
+  }
+
+  public function onEnable() { $this->getServer()->getPluginManager()->registerEvents($this, $this); }
+
+  public function getMapWithImage(int $id) : Item
+  {
+    if(isset($this->$images[$id]))
+    {
+      $item = Item::get(Item::FILLED_MAP, 0, 1);
+      $tag = $item->getNamedTag();
+      $tag->map_uuid = new StringTag("map_uuid", $id);
+      $item->setNamedTag($tag);
+    }
+    else { return Item::get(Item::AIR, 0, 0); }
+  }
+}
